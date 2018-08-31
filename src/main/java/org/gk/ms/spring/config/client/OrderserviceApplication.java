18083f5455fd16d35e0c747f60c0a48f5f4cda15
javakarthik <32713545+javakarthik@users.noreply.github.com>
2018-08-31 19:26:54 +0530

    updated java file

--- src/main/java/org/gk/ms/spring/config/client/OrderserviceApplication.java
+++ src/main/java/org/gk/ms/spring/config/client/OrderserviceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
